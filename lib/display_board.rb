e73e0b1999cb753ab7893bc63db57f4e918c192a
weiqing lee <yoweiqing@gmail.com>
2018-05-26 06:06:47 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# display_board = "   |   |   "
+# dash = "-----------"
+
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  
+end
